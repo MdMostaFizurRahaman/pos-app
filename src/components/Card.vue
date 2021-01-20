5fe59672436f4ecb8ec390af90137e6b773adf76
999itsolution <999itsolution@gmail.com>
2021-01-20 18:28:27 +0600

    Vue router added

---
+++ src/components/Card.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card text-white bg-primary mb-3">
+    <div class="card-header">{{title}}</div>
+    <div class="card-body">
+      <h5 class="card-title">${{amount}} USD</h5>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    props: {
+        title: {
+            required: true,
+            type: String,
+        },
+        amount: {
+            required: true
+        }
+    },
+
+    // data(){
+        
+    // },
+    // methods: {
+
+    // }
+}
+</script>
